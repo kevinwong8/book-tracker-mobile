6f860d70c0e0c56a861b2cf374097b5e28f80a46
kevinwong8 <kevingreg88@gmail.com>
2024-07-12 23:32:51 +0900

    lab 7

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:book_tracker/widgets/left_drawer.dart';
+import 'package:book_tracker/screens/trackerlist_form.dart';
+import 'package:book_tracker/widgets/tracker_card.dart';
 
 class MyHomePage extends StatelessWidget {
     // ignore: use_super_parameters
@@ -17,7 +20,10 @@ class MyHomePage extends StatelessWidget {
             title: const Text(
                 'Book Tracker',
             ),
+            backgroundColor: Colors.indigo,
+            foregroundColor: Colors.white,
             ),
+            drawer: const LeftDrawer(),
             body: SingleChildScrollView(
             // Widget wrapper yang dapat discroll
             child: Padding(
@@ -59,54 +65,3 @@ class MyHomePage extends StatelessWidget {
         }
 }
 
-class TrackerItem {
-    final String name;
-    final IconData icon;
-
-    TrackerItem(this.name, this.icon);
-}
-
-class TrackerCard extends StatelessWidget {
-    final TrackerItem item;
-
-    const TrackerCard(this.item, {super.key}); // Constructor
-
-    @override
-    Widget build(BuildContext context) {
-    return Material(
-        color: Colors.indigo,
-        child: InkWell(
-        // Area responsive terhadap sentuhan
-        onTap: () {
-            // Memunculkan SnackBar ketika diklik
-            ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
-        },
-        child: Container(
-            // Container untuk menyimpan Icon dan Text
-            padding: const EdgeInsets.all(8),
-            child: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                Icon(
-                    item.icon,
-                    color: Colors.white,
-                    size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                    item.name,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(color: Colors.white),
-                ),
-                ],
-            ),
-            ),
-        ),
-        ),
-    );
-    }
-}
---
+++ lib/screens/trackerlist_form.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+// TODO: Impor drawer yang sudah dibuat sebelumnya
+
+class TrackerFormPage extends StatefulWidget {
+    const TrackerFormPage({super.key});
+
+    @override
+    State<TrackerFormPage> createState() => _TrackerFormPageState();
+}
+
+class _TrackerFormPageState extends State<TrackerFormPage> {
+    final _formKey = GlobalKey<FormState>();
+    String _name = "";
+    int _page = 0;
+    String _description = "";
+    
+    
+    @override
+  
+    Widget build(BuildContext context) {
+      
+        return Scaffold(
+          appBar: AppBar(
+              title: const Center(
+                  child: Text(
+                  'Form Tambah Buku',
+                  ),
+              ),
+              backgroundColor: Colors.indigo,
+              foregroundColor: Colors.white,
+              ),
+              
+              
+              body: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                    Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                            decoration: InputDecoration(
+                                hintText: "Judul Buku",
+                                labelText: "Judul Buku",
+                                border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.0),
+                                ),
+                            ),
+                            onChanged: (String? value) {
+                                setState(() {
+                                _name = value!;
+                                });
+                            },
+                            validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                return "Judul tidak boleh kosong!";
+                                }
+                                return null;
+                            },
+                        ),
+                    ),
+                    Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Halaman",
+                        labelText: "Halaman",
+                        border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                        ),
+                    ),
+                    onChanged: (String? value) {
+                        setState(() {
+                       
+                        _page = int.tryParse(value!) ?? 0;
+                        });
+                    },
+                    validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                            return "Halaman tidak boleh kosong!";
+                        }
+                        if (int.tryParse(value) == null) {
+                            return "Halaman harus berupa angka!";
+                        }
+                        return null;
+                    },
+                ),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Deskripsi",
+                        labelText: "Deskripsi",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                        ),
+                    ),
+                    onChanged: (String? value) {
+                        setState(() {
+                       
+                        _description = value!;
+                        });
+                    },
+                    validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                        return "Deskripsi tidak boleh kosong!";
+                        }
+                        return null;
+                            },
+                        ),
+                    ),
+                      
+              Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.indigo),
+                      ),
+                      onPressed: () {
+                           if (_formKey.currentState!.validate()) {
+                              showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                      return AlertDialog(
+                                          title: const Text('Buku berhasil tersimpan'),
+                                          content: SingleChildScrollView(
+                                              child: Column(
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  children: [
+                                                      Text('Judul: $_name'),
+                                                      // TODO: Munculkan value-value lainnya
+                                                  ],
+                                              ),
+                                          ),
+                                          actions: [
+                                              TextButton(
+                                                  child: const Text('OK'),
+                                                  onPressed: () {
+                                                      Navigator.pop(context);
+                                                      _formKey.currentState!.reset();
+                                                  },
+                                              ),
+                                          ],
+                                      );
+                                  },
+                              );
+                          }
+                      },
+                  
+                      child: const Text(
+                          "Save",
+                          style: TextStyle(color: Colors.white),
+                      ),
+                  ),
+                  ),
+               ),
+              ],
+                ),
+              ),
+              ),
+          );
+    }
+}
+
